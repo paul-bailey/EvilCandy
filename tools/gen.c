95a0a0777f2a6c86ff3f506ee5682d2ec2526be6
Paul Bailey <roscidcup@falstaffandme.com>
2025-04-15 23:16:04 -0700

    Support comments in tools/instruction, then comment the hell out of them.

--- tools/gen.c
+++ tools/gen.c
@@ -1,10 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 #include <errno.h>
 #include <stdbool.h>
 
 static char buf[1024];
+
+static int
+istokchar(int c)
+{
+        /* isdigit in case we add things like PUSH_3 */
+        return isupper(c) || isdigit(c) || c == '_';
+}
+
+static int
+iseol(int c)
+{
+        return c == '#' || c == '\n' || c == '\0';
+}
+
+static char *
+skipws(char *s)
+{
+        while (isspace((int)(*s)))
+                s++;
+        return s;
+}
+
+/* return: 1 for 'buf has 1 token', 0 for 'end of file' */
+static int
+next_instruction(void)
+{
+        static char *line = NULL;
+        static size_t len = 0;
+
+        char *src, *dst;
+        ssize_t res;
+
+        do {
+                res = getline(&line, &len, stdin);
+                if (res < 0) {
+                        if (errno) {
+                                perror("getline failed");
+                                exit(1);
+                        }
+                        return 0;
+                }
+
+                src = skipws(line);
+        } while (iseol(*src));
+
+        dst = buf;
+        while (istokchar(*src)) {
+                if (dst >= &buf[sizeof(buf)-1]) {
+                        fprintf(stderr, "buffer would overflow\n");
+                        exit(1);
+                }
+                *dst++ = *src++;
+        }
+        if (dst == buf) {
+                fprintf(stderr, "Expected: alphanumeric character\n");
+                exit(1);
+        }
+
+        *dst++ = '\0';
+
+        if (isdigit((int)(buf[0]))) {
+                fprintf(stderr, "first character of name cannot be number\n");
+                exit(1);
+        }
+
+        /* We should be getting just one non-comment token per line */
+        src = skipws(src);
+
+        if (!iseol(*src)) {
+                fprintf(stderr, "Unexpected tokens in input");
+                exit(1);
+        }
+        return 1;
+}
 
 static void
 prlower(void)
@@ -33,7 +108,7 @@ dis(void)
                " * used by disassemble.c\n"
                " * (see tools/gen.c, tools/instructions)\n"
                " */\n");
-        while ((res = scanf("%s", buf)) == 1) {
+        while ((res = next_instruction()) == 1) {
                 printf("        \"");
                 prupper();
                 printf("\",\n");
@@ -56,7 +131,7 @@ def(void)
         printf("#ifndef %s\n", excl);
         printf("#define %s\n", excl);
         printf("enum {\n");
-        while ((res = scanf("%s", buf)) == 1) {
+        while ((res = next_instruction()) == 1) {
                 printf("        INSTR_");
                 prupper();
                 if (first) {
@@ -85,7 +160,7 @@ jump(void)
                " * used by vm.c\n"
                " * (see tools/gen.c, tools/instructions)\n"
                " */\n");
-        while ((res = scanf("%s", buf)) == 1) {
+        while ((res = next_instruction()) == 1) {
                 printf("        do_");
                 prlower();
                 putchar(',');
